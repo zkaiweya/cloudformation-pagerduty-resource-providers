32aa5fbbaf590f80f4f3644bc51bd89771437617
Kai Wen Yang <zkaiweya@amazon.com>
2024-01-28 13:01:18 +0000

    push dummy commit to trigger codebuild

--- PagerDuty-Services-Integration/src/handlers.ts
+++ PagerDuty-Services-Integration/src/handlers.ts
@@ -38,7 +38,7 @@ class Resource extends AbstractPagerDutyResource<ResourceModel, IntegrationPaylo
     }
 
     async list(model: ResourceModel, typeConfiguration?: TypeConfigurationModel): Promise<ResourceModel[]> {
-        // no api to list integration, use Read as List.
+        // no api to list integration, use Read as List
         try {
             const response = await new PagerDutyClient(typeConfiguration?.pagerDutyAccess.token, this.userAgent).doRequest<{
                 integration: IntegrationPayload
