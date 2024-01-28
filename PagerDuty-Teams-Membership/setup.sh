a36f9a85250f7d17e5280a97a4210850476c4eb9
Kai Wen Yang <zkaiweya@amazon.com>
2024-01-28 14:23:27 +0000

    fix teams membership

--- PagerDuty-Teams-Membership/setup.sh
+++ PagerDuty-Teams-Membership/setup.sh
@@ -3,5 +3,5 @@
 # Set up any prerequisites needed for cfn test
 #
 mkdir -p inputs
-cat example_inputs/inputs_1_create.json | sed "s/PAGER_DUTY_TEAM_ID/${PAGER_DUTY_TEAM_ID}/g" | sed "s/PAGER_DUTY_USER_ID/${PAGER_DUTY_USER_ID}/g" > inputs/inputs_1_create.json
-cat test/integ-template.yml | sed "s/PAGER_DUTY_TEAM_ID/${PAGER_DUTY_TEAM_ID}/g" | sed "s/PAGER_DUTY_USER_ID/${PAGER_DUTY_USER_ID}/g" > test/integ.yml
+cat example_inputs/inputs_1_create.json | sed "s/PAGER_DUTY_TEAM_ID_2/${PAGER_DUTY_TEAM_ID_2}/g" | sed "s/PAGER_DUTY_USER_ID_2/${PAGER_DUTY_USER_ID_2}/g" > inputs/inputs_1_create.json
+cat test/integ-template.yml | sed "s/PAGER_DUTY_TEAM_ID_2/${PAGER_DUTY_TEAM_ID_2}/g" | sed "s/PAGER_DUTY_USER_ID_2/${PAGER_DUTY_USER_ID_2}/g" > test/integ.yml
